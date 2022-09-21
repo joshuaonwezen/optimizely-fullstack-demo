80685617fabefadb9d7f4bcdda80ca9d84e2176b
joshua <joshua.onwezen@gmail.com>
2022-09-21 10:52:30 +0200

    update logo

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -55,7 +55,7 @@ const navigation = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Optimizely
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
